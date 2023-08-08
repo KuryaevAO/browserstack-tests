1911e31dd6b78800ec0c96cee5d9e1f145206229
KuryaevAO <aleksandr.kuryaev@simbirsoft.com>
2023-08-08 10:04:20 +0300

    temp changes7

--- src/test/java/drivers/BrowserstackMobileDriver.java
+++ src/test/java/drivers/BrowserstackMobileDriver.java
@@ -7,18 +7,17 @@ import org.aeonbits.owner.ConfigFactory;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.remote.DesiredCapabilities;
 
-
 import java.net.MalformedURLException;
 import java.net.URL;
 
 public class BrowserstackMobileDriver implements WebDriverProvider {
 
-    CredentialsConfig credentials =
-            ConfigFactory.create(CredentialsConfig.class);
+    final CredentialsConfig config =
+            ConfigFactory.create(CredentialsConfig.class, System.getProperties());
 
-    public static URL getBrowserstackUrl() {
+    public URL getBrowserstackUrl() {
         try {
-            return new URL("http://hub.browserstack.com/wd/hub");
+            return new URL(config.baseUrl());
         } catch (MalformedURLException e) {
             throw new RuntimeException(e);
         }
@@ -27,20 +26,20 @@ public class BrowserstackMobileDriver implements WebDriverProvider {
     public WebDriver createDriver(DesiredCapabilities desiredCapabilities) {
 
         // Set your access credentials
-        desiredCapabilities.setCapability("browserstack.user", credentials.user());
-        desiredCapabilities.setCapability("browserstack.key", credentials.key());
+        desiredCapabilities.setCapability("browserstack.user", config.user());
+        desiredCapabilities.setCapability("browserstack.key", config.key());
 
         // Set URL of the application under test
-        desiredCapabilities.setCapability("app", credentials.app());
+        desiredCapabilities.setCapability("app", config.app());
 
         // Specify device and os_version for testing
-        desiredCapabilities.setCapability("device", credentials.device());
-        desiredCapabilities.setCapability("os_version", credentials.os_version());
+        desiredCapabilities.setCapability("device", config.device());
+        desiredCapabilities.setCapability("os_version", config.os_version());
 
         // Set other BrowserStack capabilities
-        desiredCapabilities.setCapability("project", credentials.project());
-        desiredCapabilities.setCapability("build", credentials.build());
-        desiredCapabilities.setCapability("name", credentials.name());
+        desiredCapabilities.setCapability("project", config.project());
+        desiredCapabilities.setCapability("build", config.build());
+        desiredCapabilities.setCapability("name", config.name());
 
         return new AndroidDriver(getBrowserstackUrl(), desiredCapabilities);
     }
